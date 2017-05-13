1f76f3a52e63c76c1cf5d2a7080fdd90157b058a
anatolorzecki <fejder@fejder.com>
2017-05-14 00:49:38 +0200

    master upload

---
+++ despolszczator.py
@@ -0,0 +1,41 @@
+# despolszczator 1.0
+
+import os
+from tkinter import *
+import unicodedata
+
+window = Tk()
+window.title("deSpolszczator 1.0")
+window.resizable(width=False, height=False)
+
+
+def swapper():
+    file_names = os.listdir(file_path.get())
+    print(file_path.get())
+    letter_swap(file_names)  # listing folderow
+
+
+def letter_swap(names):
+    path = file_path.get()
+    for name in names:
+        old_name = name
+        if name[:1] == '.':
+            continue
+        else:
+            nfkd_form = unicodedata.normalize('NFKD', name).replace(u'ł', 'l').replace(u' ', '_')
+            wo_pl_letters = nfkd_form.encode('utf-8', 'ignore').decode('ASCII', 'ignore')
+            print(wo_pl_letters)
+            os.rename(path+old_name, path+str(wo_pl_letters))
+
+l1 = Label(window, text="Path to execute:")
+l1.grid(row=0, column=0)
+
+file_path = StringVar()
+e1 = Entry(window, textvariable=file_path)
+e1.grid(row=0, column=1, columnspan=3)
+file_path.set("")
+
+b1 = Button(window, text="Clean this shit out!", command=swapper)
+b1.grid(row=0, column=4)
+
+window.mainloop()
